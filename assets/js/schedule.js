1428994b094fec8fa97f8a8947883424285eba7d
Alejandro Cortez <102841726+Muzan67@users.noreply.github.com>
2022-08-16 21:47:23 -0400

    completed module 3

---
+++ assets/js/schedule.js
@@ -0,0 +1,6 @@
+require("bootstrap");
+const { createLoremIpsum, dateConverter } = require("./helpers");
+const createEl = require("./domMethods");
+
+if (window.location.href.indexOf("schedule") > -1) {
+}
